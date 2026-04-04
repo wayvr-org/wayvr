a3460fbe7055f1ef2b3f927fee44283018c62a6c
Aleksander <aleksander@oo8.dev>
2026-04-04 22:08:44 +0200

    dash-frontend: use SettingsTab trait for each settings tab

--- dash-frontend/src/tab/settings/tab_look_and_feel.rs
+++ dash-frontend/src/tab/settings/tab_look_and_feel.rs
@@ -1,20 +1,30 @@
 use crate::tab::settings::{
-	SettingType,
-	macros::{MacroParams, options_category, options_checkbox, options_dropdown, options_slider_f32},
+	SettingType, SettingsMountParams, SettingsTab,
+	macros::{options_category, options_checkbox, options_dropdown, options_slider_f32},
 };
-use wgui::layout::WidgetID;
 
-pub fn mount(mp: &mut MacroParams, parent: WidgetID) -> anyhow::Result<()> {
-	let c = options_category(mp, parent, "APP_SETTINGS.LOOK_AND_FEEL", "dashboard/palette.svg")?;
-	options_dropdown::<wlx_common::locale::Language>(mp, c, &SettingType::Language)?;
-	options_checkbox(mp, c, SettingType::HideUsername)?;
-	options_checkbox(mp, c, SettingType::HideGrabHelp)?;
-	options_slider_f32(mp, c, SettingType::UiAnimationSpeed, 0.5, 5.0, 0.1)?; // min, max, step
-	options_slider_f32(mp, c, SettingType::UiGradientIntensity, 0.0, 1.0, 0.05)?; // min, max, step
-	options_slider_f32(mp, c, SettingType::UiRoundMultiplier, 0.5, 5.0, 0.1)?;
-	options_checkbox(mp, c, SettingType::SetsOnWatch)?;
-	options_slider_f32(mp, c, SettingType::GridOpacity, 0.0, 1.0, 0.05)?; // min, max, step
-	options_checkbox(mp, c, SettingType::UsePassthrough)?;
-	options_checkbox(mp, c, SettingType::Clock12h)?;
-	Ok(())
+pub struct State {}
+
+impl SettingsTab for State {}
+
+impl State {
+	pub fn mount(par: SettingsMountParams) -> anyhow::Result<State> {
+		let c = options_category(
+			par.mp,
+			par.parent_id,
+			"APP_SETTINGS.LOOK_AND_FEEL",
+			"dashboard/palette.svg",
+		)?;
+		options_dropdown::<wlx_common::locale::Language>(par.mp, c, &SettingType::Language)?;
+		options_checkbox(par.mp, c, SettingType::HideUsername)?;
+		options_checkbox(par.mp, c, SettingType::HideGrabHelp)?;
+		options_slider_f32(par.mp, c, SettingType::UiAnimationSpeed, 0.5, 5.0, 0.1)?; // min, max, step
+		options_slider_f32(par.mp, c, SettingType::UiGradientIntensity, 0.0, 1.0, 0.05)?; // min, max, step
+		options_slider_f32(par.mp, c, SettingType::UiRoundMultiplier, 0.5, 5.0, 0.1)?;
+		options_checkbox(par.mp, c, SettingType::SetsOnWatch)?;
+		options_slider_f32(par.mp, c, SettingType::GridOpacity, 0.0, 1.0, 0.05)?; // min, max, step
+		options_checkbox(par.mp, c, SettingType::UsePassthrough)?;
+		options_checkbox(par.mp, c, SettingType::Clock12h)?;
+		Ok(State {})
+	}
 }
--- dash-frontend/src/tab/settings/tab_troubleshooting.rs
+++ dash-frontend/src/tab/settings/tab_troubleshooting.rs
@@ -1,45 +1,55 @@
 use crate::tab::settings::{
-	Task,
-	macros::{MacroParams, options_category, options_danger_button},
+	SettingsMountParams, SettingsTab, Task,
+	macros::{options_category, options_danger_button},
 };
-use wgui::layout::WidgetID;
 
-pub fn mount(mp: &mut MacroParams, parent: WidgetID) -> anyhow::Result<()> {
-	let c = options_category(mp, parent, "APP_SETTINGS.TROUBLESHOOTING", "dashboard/cpu.svg")?;
-	options_danger_button(
-		mp,
-		c,
-		"APP_SETTINGS.RESET_PLAYSPACE",
-		"dashboard/recenter.svg",
-		Task::ResetPlayspace,
-	)?;
-	options_danger_button(
-		mp,
-		c,
-		"APP_SETTINGS.CLEAR_PIPEWIRE_TOKENS",
-		"dashboard/display.svg",
-		Task::ClearPipewireTokens,
-	)?;
-	options_danger_button(
-		mp,
-		c,
-		"APP_SETTINGS.CLEAR_SAVED_STATE",
-		"dashboard/binary.svg",
-		Task::ClearSavedState,
-	)?;
-	options_danger_button(
-		mp,
-		c,
-		"APP_SETTINGS.DELETE_ALL_CONFIGS",
-		"dashboard/circle.svg",
-		Task::DeleteAllConfigs,
-	)?;
-	options_danger_button(
-		mp,
-		c,
-		"APP_SETTINGS.RESTART_SOFTWARE",
-		"dashboard/refresh.svg",
-		Task::RestartSoftware,
-	)?;
-	Ok(())
+pub struct State {}
+
+impl SettingsTab for State {}
+
+impl State {
+	pub fn mount(par: SettingsMountParams) -> anyhow::Result<Self> {
+		let c = options_category(
+			par.mp,
+			par.parent_id,
+			"APP_SETTINGS.TROUBLESHOOTING",
+			"dashboard/cpu.svg",
+		)?;
+		options_danger_button(
+			par.mp,
+			c,
+			"APP_SETTINGS.RESET_PLAYSPACE",
+			"dashboard/recenter.svg",
+			Task::ResetPlayspace,
+		)?;
+		options_danger_button(
+			par.mp,
+			c,
+			"APP_SETTINGS.CLEAR_PIPEWIRE_TOKENS",
+			"dashboard/display.svg",
+			Task::ClearPipewireTokens,
+		)?;
+		options_danger_button(
+			par.mp,
+			c,
+			"APP_SETTINGS.CLEAR_SAVED_STATE",
+			"dashboard/binary.svg",
+			Task::ClearSavedState,
+		)?;
+		options_danger_button(
+			par.mp,
+			c,
+			"APP_SETTINGS.DELETE_ALL_CONFIGS",
+			"dashboard/circle.svg",
+			Task::DeleteAllConfigs,
+		)?;
+		options_danger_button(
+			par.mp,
+			c,
+			"APP_SETTINGS.RESTART_SOFTWARE",
+			"dashboard/refresh.svg",
+			Task::RestartSoftware,
+		)?;
+		Ok(State {})
+	}
 }
